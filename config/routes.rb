bc9a5904e080e4435b24de86c0afd202be7f98b2
Carlos <carlos.delgado@hotmail.it>
2020-02-16 22:25:07 +0100

    created first test, expect to be on the new article page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'articles/index'   
   root controller: :articles, action: :index
-  resources :articles, only: [:index, :show]
+  resources :articles
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
